5416786485f0e811a98780fe9cd51ab3dc9eb48c
Levko Kravets <kravets@qarea.com>
2015-11-10 11:25:04 +0200

    Added stubs for all steps; some ui improvements

---
+++ app/front/scripts/controllers/download-package.js
@@ -0,0 +1,11 @@
+;(function(angular) {
+
+  angular.module('Application')
+    .controller('DownloadPackageController', [
+      '$scope',
+      function($scope) {
+
+      }
+    ]);
+
+})(angular);

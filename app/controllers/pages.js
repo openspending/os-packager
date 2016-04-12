95fdec84b709c6aabee4759641dc9af659a4f8ee
Levko Kravets <levko.ne@gmail.com>
2016-04-12 08:56:45 +0300

    openspending/openspending#926 Fix links and resource urls to work with relative path

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -3,24 +3,39 @@
 var services = require('../services');
 var _ = require('underscore');
 
+function getBasePath(config) {
+  var result = config.get('basePath');
+  if (_.isUndefined(result) || _.isNull(result) || (result == '')) {
+    return '';
+  }
+  result = '' + result;
+  if (result[0] != '/') {
+    result = '/' + result;
+  }
+  return result;
+}
+
 module.exports.main = function(req, res) {
   var config = req.app.get('config');
+  var basePath = getBasePath(config);
+
   res.render('pages/main.html', {
     conductor: config.get('conductor'),
-    basePath: config.get('basePath'),
+    basePath: basePath,
     title: 'Create a Fiscal Data Package'
   });
 };
 
 module.exports.landing = function(req, res) {
   var config = req.app.get('config');
+  var basePath = getBasePath(config);
   var firstStep = _.first(services.data.steps);
 
   res.render('pages/landing.html', {
     conductor: config.get('conductor'),
-    basePath: config.get('basePath'),
+    basePath: basePath,
     title: 'OS Packager',
-    getStartedUrl: firstStep.route
+    getStartedUrl: (basePath ? '/' + basePath : '') + firstStep.route
   });
 };
 

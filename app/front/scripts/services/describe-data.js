83baf957b972dea56d244287f089b05fff80185a
Adam Kariv <adam.kariv@gmail.com>
2017-01-20 22:01:16 +0200

    Adapt to async interface in os-types

--- app/front/scripts/services/describe-data.js
+++ app/front/scripts/services/describe-data.js
@@ -47,71 +47,74 @@ angular.module('Application')
           delete field.slug;
         });
         var fdp = new OSTypes().fieldsToModel(fields);
-        if (fdp.errors) {
+        var promise = fdp.promise || Promise.resolve();
+        return promise.then(function() {
+          if (fdp.errors) {
+            _.forEach(fields, function(field) {
+              var fieldErrors = fdp.errors.perField[field.name];
+              if (fieldErrors) {
+                field.errors = fieldErrors;
+              }
+            });
+          }
           _.forEach(fields, function(field) {
-            var fieldErrors = fdp.errors.perField[field.name];
-            if (fieldErrors) {
-              field.errors = fieldErrors;
-            }
-          });
-        }
-        _.forEach(fields, function(field) {
-          if (fdp.schema && fdp.schema.fields &&
-            fdp.schema.fields[field.name]) {
-            var schemaField = fdp.schema.fields[field.name];
-            field.additionalOptions = schemaField.options;
-            if (!field.options) {
+            if (fdp.schema && fdp.schema.fields &&
+              fdp.schema.fields[field.name]) {
+              var schemaField = fdp.schema.fields[field.name];
+              field.additionalOptions = schemaField.options;
+              if (!field.options) {
+                field.options = {};
+              }
+              _.forEach(field.additionalOptions, function(option) {
+                if (option.name == 'currency') {
+                  option.values = _.map(UtilsService.getCurrencies(),
+                    function(item) {
+                      return {
+                        name: item.code + ' ' + item.name,
+                        value: item.code
+                      };
+                    });
+                  option.defaultValue =
+                    UtilsService.getDefaultCurrency().code;
+                }
+
+                var existing = field.options[option.name];
+                if (_.isUndefined(existing)) {
+                  if (_.has(option, 'defaultValue')) {
+                    field.options[option.name] = option.defaultValue;
+                  }
+                }
+              });
+            } else {
+              field.additionalOptions = [];
               field.options = {};
             }
-            _.forEach(field.additionalOptions, function(option) {
-              if (option.name == 'currency') {
-                option.values = _.map(UtilsService.getCurrencies(),
-                  function(item) {
-                    return {
-                      name: item.code + ' ' + item.name,
-                      value: item.code
-                    };
-                  });
-                option.defaultValue =
-                  UtilsService.getDefaultCurrency().code;
-              }
+          });
 
-              var existing = field.options[option.name];
-              if (_.isUndefined(existing)) {
-                if (_.has(option, 'defaultValue')) {
-                  field.options[option.name] = option.defaultValue;
-                }
-              }
-            });
-          } else {
-            field.additionalOptions = [];
-            field.options = {};
-          }
-        });
-
-        state.errors = [];
-        if (fdp.errors) {
-          if (fdp.errors.general) {
-            _.every(fdp.errors.general, function(msg) {
-              state.errors.push({msg: msg});
-            });
-          }
-          if (fdp.errors.perField) {
-            _.forEach(fdp.errors.perField, function(msgs, field) {
-              _.every(msgs, function(msg) {
-                state.errors.push({field: field, msg: msg});
+          state.errors = [];
+          if (fdp.errors) {
+            if (fdp.errors.general) {
+              _.every(fdp.errors.general, function(msg) {
+                state.errors.push({msg: msg});
               });
-            });
+            }
+            if (fdp.errors.perField) {
+              _.forEach(fdp.errors.perField, function(msgs, field) {
+                _.every(msgs, function(msg) {
+                  state.errors.push({field: field, msg: msg});
+                });
+              });
+            }
           }
-        }
 
-        state.status = ValidationService.validateRequiredConcepts(
-          fdp.errors,
-          PackageService.getResources());
+          state.status = ValidationService.validateRequiredConcepts(
+            fdp.errors,
+            PackageService.getResources());
 
-        PreviewDataService.update();
+          PreviewDataService.update();
 
-        return state;
+          return state;
+        });
       };
 
       return result;

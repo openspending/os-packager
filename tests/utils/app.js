b0206ee35c8a7e7ac99d3701e250b70888d590e6
Adam Kariv <adam.kariv@gmail.com>
2017-07-12 14:36:01 +0300

    Fix port to be 5000

--- tests/utils/app.js
+++ tests/utils/app.js
@@ -44,8 +44,7 @@ exports.start = function(done) {
     // Run the server
     start().then(function(app) {
       exports.app = app;
-      var port = app.get('port');
-      Browser.localhost('127.0.0.1', port);
+      Browser.localhost('127.0.0.1', 5000);
       exports.browser = new Browser({
         maxWait: 5000
       });

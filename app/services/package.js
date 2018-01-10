b8b34994fdf38936952cead3e3503f4ba99accff
Adam Kariv <adam.kariv@gmail.com>
2018-01-10 18:56:46 +0200

    Update datapackage to v1, fix validation errors

--- app/services/package.js
+++ app/services/package.js
@@ -92,7 +92,7 @@ function getFiscalDataPackageSchema(useProxy) {
 function validateDataPackage(descriptor, schema) {
   return datapackage.validate(descriptor, schema, true)
     .then(function(validation) {
-      return validation == true;
+      return validation;
     });
 }
 
